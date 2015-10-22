71d628f7830e1e388a8b6631f9694783782912a9
yashoswal <yashoswal@users.noreply.github.com>
2015-10-22 11:47:15 -0700

    Commenting in Random forest

--- RandomForestProject/src/main/scala/RandomForest.scala
+++ RandomForestProject/src/main/scala/RandomForest.scala
@@ -19,60 +19,48 @@ object SimpleApp {
 	Logger.getLogger("akka").setLevel(Level.OFF)
 	val conf = new SparkConf().setAppName("RandomForest")
     	val sc = new SparkContext(conf)	
-		// Load and parse the data file.
+	// Load and parse the data file.
 	val rawData = sc.textFile("/home/yash/Downloads/joined click fraud data/counts_with_time_60_without_partnerid_feb9")
 	val rawData1 = sc.textFile("/home/yash/Downloads/joined click fraud data/counts_with_time_60_without_partnerid_feb23")
 
-val trainingData = getLabeledPoints_observe_ok(rawData)
-val testData = getLabeledPoints_observe_ok(rawData1)
-//val splits = data.randomSplit(Array(0.7, 0.3))
-//val (trainingData, testData) = (splits(0), splits(1))
+	val trainingData = getLabeledPoints_observe_ok(rawData)
+	val testData = getLabeledPoints_observe_ok(rawData1)
 
-// Train a RandomForest model.
-/*val treeStrategy = Strategy.defaultStrategy("Classification")
-val numTrees = 20 // Use more in practice.
-val featureSubsetStrategy = "auto" // Let the algorithm choose.
-val model = RandomForest.trainClassifier(trainingData,treeStrategy, numTrees, featureSubsetStrategy, seed = 12345)
-*/
 
-val numClasses = 2
-val categoricalFeaturesInfo = Map[Int, Int]((4,10))
-//val categoricalFeaturesInfo = Map[Int, Int]()
-val numTrees = 30 // Use more in practice.
-val featureSubsetStrategy = "auto" // Let the algorithm choose.
-val impurity = "entropy"
-val maxDepth = 5
-val maxBins = 32
+	val numClasses = 2
+	val categoricalFeaturesInfo = Map[Int, Int]((4,10))
+	val numTrees = 30 
+	val featureSubsetStrategy = "auto" 
+	val impurity = "entropy"
+	val maxDepth = 5
+	val maxBins = 32
+	
+	val model = RandomForest.trainClassifier(trainingData, numClasses, categoricalFeaturesInfo,
+	  numTrees, featureSubsetStrategy, impurity, maxDepth, maxBins)
+	val testErr = testData.map { point =>val prediction = model.predict(point.features)
+	  if (point.label == prediction) 1.0 else 0.0
+	}.mean()
+	println("Test Error = " + testErr)
+	val metrics = getMetrics(model,testData)
+	println(metrics.confusionMatrix)
+	}
 
-val model = RandomForest.trainClassifier(trainingData, numClasses, categoricalFeaturesInfo,
-  numTrees, featureSubsetStrategy, impurity, maxDepth, maxBins)
-// Evaluate model on test instances and compute test error
-val testErr = testData.map { point =>val prediction = model.predict(point.features)
-  if (point.label == prediction) 1.0 else 0.0
-}.mean()
-println("Test Error = " + testErr)
-//println("Learned Random Forest:n" + model.toDebugString)
-val metrics = getMetrics(model,testData)
-println(metrics.confusionMatrix)
+//Metrics after running the model	
+def getMetrics(model: RandomForestModel, data: RDD[LabeledPoint]):MulticlassMetrics = {
+	val predictionsAndLabels = data.map { point =>val prediction = model.predict(point.features)
+ 				   (point.label, prediction)
+				}
+	val testErr = predictionsAndLabels.filter(r => r._1 != r._2).count.toDouble / data.count()
+	println("Test Error = " + testErr)
+	new MulticlassMetrics(predictionsAndLabels)
 }
 
-def getMetrics(model: RandomForestModel, data: RDD[LabeledPoint]):MulticlassMetrics = {
-  		//val predictionsAndLabels = data.map(example =>(model.predict(example.features), example.label))
-		val predictionsAndLabels = data.map { point =>val prediction = model.predict(point.features)
- 					   (point.label, prediction)
-					}
-		//predictionsAndLabels.saveAsTextFile("/home/yash/Downloads/click-fraud-data/output")
-		val testErr = predictionsAndLabels.filter(r => r._1 != r._2).count.toDouble / data.count()
-		println("Test Error = " + testErr)
-		new MulticlassMetrics(predictionsAndLabels)
-  		//new BinaryClassificationMetrics(predictionsAndLabels)
-   }
+//Labeled points of the raw data
 def getLabeledPoints_observe_ok(rawData:RDD[String]):RDD[LabeledPoint] = {
 	rawData.map { line => val values = line.split(',').map(_.toDouble)
-		if(values(7)==1.0){values(7)=0.0}else if(values(7)==2.0){values(7)=1.0}
-		//if(values.last==1.0){values(8)=0.0}else if(values.last==2.0){values(8)=1.0}
-  		val featureVector = Vectors.dense(values.init) 
-  		val label = values.last
-  		LabeledPoint(label, featureVector) }
+	if(values(7)==1.0){values(7)=0.0}else if(values(7)==2.0){values(7)=1.0}
+  	val featureVector = Vectors.dense(values.init) 
+  	val label = values.last
+  	LabeledPoint(label, featureVector) }
 }
 }
